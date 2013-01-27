aa38ed58dc7aa6ff1daf16f08d46ad010a6161b0
Albert-Jan Roskam <fomcl@yahoo.com>
2013-01-27 21:53:50 +0100

    Wrote setup script

--- cWriterow/readme.txt
+++
@@ -1,4 +0,0 @@
-# cWriterow is a faster C version of the Python pyWriterow method.
-# It requires that Cython + a C compiler be installed (http://docs.cython.org/src/quickstart/install.html)		
-easy_install cython  # requires setuptools
-python setup.py build_ext --inplace
--- setup.py
+++ setup.py
@@ -6,25 +6,32 @@ from distutils.core import setup
 import os.path
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    return open(os.path.join(os.path.dirname(__file__), fname)).read().strip()
 
-def version(fname='VERSION'):
-    p = os.path.realpath(__file__)
-    return open(os.path.join(os.path.dirname(p), fname)).read().strip()
+#      py_modules=['__init__', 'error', 'generic', 'header',
+#                  'savHeaderReader', 'savReader','savWriter'],
+email = "@".join(["fomcl", "yahoo.com"])
 
+spssio = ['include/*.*', 'win64/*.*', 'macos/*.*', 'win32/*.*', 'sol64/*.*', 'lin32/*.*', 'lin64/*.*', 
+          'documents/*.*', 'hpux_it/*.*', 'zlinux64/*.*', 'aix64/*.*', 'license/*.*']
+
+spssio = ['spssio/include/*.*', 'spssio/win64/*.*', 'spssio/macos/*.*', 
+          'spssio/win32/*.*', 'spssio/sol64/*.*', 'spssio/lin32/*.*', 
+          'spssio/lin64/*.*', 'spssio/documents/*.*', 'spssio/hpux_it/*.*', 
+          'spssio/zlinux64/*.*', 'spssio/aix64/*.*', 'spssio/license/*.*']
 setup(name='savReaderWriter',
-      version='3.1.1',
+      version=read('VERSION'),
       description='Read and write SPSS files',
       author='Albert-Jan Roskam',
-      author_email='fomcl@yahoo.com',
+      author_email=email,
       maintainer='Albert-Jan Roskam',
-      maintainer_email='fomcl@yahoo.com',
+      maintainer_email=email,
       license='MIT',
       long_description=read('README'),
       platforms=['Windows', 'Mac', 'Linux/POSIX'],
       url='https://bitbucket.org/fomcl/savReaderWriter',
-      py_modules=['__init__', 'error', 'generic', 'header',
-                  'savHeaderReader', 'savReader','savWriter'],
+      packages=['savReaderWriter'],
+      package_data={'savReaderWriter': spssio + ['cWriterow/*.*', 'documentation/*.*', 'build/*.*']},
       classifiers=['Development Status :: 4 - Beta',
                    'Intended Audience :: Developers',
                    'License :: OSI Approved :: MIT License',
