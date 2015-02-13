b7657716384867cbed3579b6ae379679a65b4a71
fomcl <fomcl@yahoo.com>
2015-02-13 13:07:47 +0100

    wrote some, but not yet all, unittests for multiple response definitions, see issue #23

--- savReaderWriter/unit_tests/test_SavWriter_Mrsets.py
+++ savReaderWriter/unit_tests/test_SavWriter_Mrsets.py
@@ -6,6 +6,9 @@ import os
 import tempfile
 from savReaderWriter import SavWriter
 
+
+# TODO: tests for ioUtf8=True
+# See also issue #26
 records = [[1000, b"higher"], [2000, b"university"], 
            [2000, b"\xe5\xa4\xa7\xe5\xad\xa6 (chinese university)"]]
 
@@ -16,13 +19,48 @@ varTypes = {b"income": 0, b"educ": 30}
 categorical =  {b"setType": b"C", 
                 b"label": b"labelC",
                 b"varNames": [b"salary", b"educ"]}
-dichotomous1 = {b"setType": b"D", b"label": b"labelD",
+dichotomous = {b"setType": b"D", b"label": b"labelD",
                 b"varNames": [b"salary", b"educ"], 
                 b"countedValue": b"Yes"}
 multRespDefs = {b"testSetC": categorical, 
-                b"testSetD1": dichotomous1}
+                b"testSetD1": dichotomous}
 #multRespDefs = {b"testSetD1": dichotomous1}
 #multRespDefs = {b"testSetC": categorical} 
-with SavWriter(savFileName, varNames, varTypes, multRespDefs=multRespDefs) as writer:
-    writer.writerows(records)
+
+class Test_Mrsets(unittest.TestCase):
+
+    """
+    Write multiple response sets (dichotomy and categorical;
+    extended ones can't be written, only read)
+    """  
+
+    def setUp(self):
+        self.kwargs = dict(savFileName=savFileName, 
+                           varNames=varNames, 
+                           varTypes=varTypes)
+
+    def test_multRespDefs_dichotomy(self):
+        self.kwargs.update(dict(multRespDefs={b"testSetD1": dichotomous}))
+        with SavWriter(**self.kwargs) as writer:
+            writer.writerows(records)
+
+    def test_multRespDefs_categorical(self):
+        self.kwargs.update(dict(multRespDefs={b"testSetD1": categorical}))
+        with SavWriter(**self.kwargs) as writer:
+            writer.writerows(records)
+
+    def test_multRespDefs_both(self):
+        self.kwargs.update(dict(multRespDefs={b"testSetC": categorical, 
+                                              b"testSetD1": dichotomous}))
+        with SavWriter(**self.kwargs) as writer:
+            writer.writerows(records)
+
+    def tearDown(self):
+        try:
+            os.remove(savFileName)
+        except:
+            pass 
+
+if __name__ == "__main__":
+    unittest.main()
 
