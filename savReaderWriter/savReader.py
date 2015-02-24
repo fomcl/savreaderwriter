65487190c969baebebac2e524029b95bf949f2de
fomcl <fomcl@yahoo.com>
2015-02-24 22:58:42 +0100

    fixed bug with N format in Python 2, but not yet with Python 3

--- savReaderWriter/savReader.py
+++ savReaderWriter/savReader.py
@@ -188,7 +188,7 @@ class SavReader(Header):
         """Helper function for formatValues function. Determines whether
         iterating over each individual value is really needed"""
         hasDates = bool(set(self.bareformats.values()) & set(supportedDates))
-        hasNfmt = b"N" in self.bareformats
+        hasNfmt = b"N" in list(self.bareformats.values())
         hasRecodeSysmis = self.recodeSysmisTo is not None
         items = [hasDates, hasNfmt, hasRecodeSysmis, self.ioUtf8_]
         return False if any(items) else True
@@ -213,9 +213,9 @@ class SavReader(Header):
                 else:
                     record[i] = self.recodeSysmisTo
                 # format N-type values (=numerical with leading zeroes)
-                if bareformat_ == "N":
+                if bareformat_ == b"N":
                     #record[i] = str(value).zfill(varWid)
-                    record[i] = "%%0%dd" % varWid % value  #15 x faster (zfill)
+                    record[i] = bytez("%%0%dd" % varWid % value)  #15 x faster (zfill)
                 # convert SPSS dates to ISO dates
                 elif bareformat_ in supportedDates:
                     fmt = supportedDates[bareformat_]
---
+++ savReaderWriter/unit_tests/test_SavReader_leading_zero.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from contextlib import closing
+import unittest
+import savReaderWriter as rw
+
+# N format = numerical with leading zeroes
+class Test_N_Format(unittest.TestCase):
+
+    def setUp(self):
+        self.savFileName = "../savReaderWriter/test_data/leading_zero.sav"
+
+    def test_n_format(self):
+        with closing(rw.SavReader(self.savFileName)) as reader:
+            records_got = reader.all()
+        records_expected = [[b'0000000001'], [b'0000000002']]
+        self.assertEqual(records_expected, records_got)
+
+    def test_n_format_ioUtf8(self):
+        with closing(rw.SavReader(self.savFileName, ioUtf8=True)) as reader:
+            records_got = reader.all()
+        records_expected = [[u'0000000001'], [u'0000000002']]
+        self.assertEqual(records_expected, records_got)
+
+if __name__ == "__main__":
+    unittest.main()
